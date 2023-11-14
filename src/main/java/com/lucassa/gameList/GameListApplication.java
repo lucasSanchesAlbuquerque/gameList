11a7400e6026c9fc200ff5f61a776501f781f177
Lucas SA <lusanchesx@gmail.com>
2023-11-13 21:29:34 -0300

    Projeto criado

---
+++ src/main/java/com/lucassa/gameList/GameListApplication.java
@@ -0,0 +1,13 @@
+package com.lucassa.gameList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameListApplication.class, args);
+	}
+
+}
